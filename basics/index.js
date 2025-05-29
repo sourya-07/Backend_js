557a23c54120f1802fafc1ec3dd4d67763d1c6c7
Sourya <souryagupta2005@gmail.com>
2025-05-29 23:07:08 +0530

    Understood about the .env working and basics of json

--- basics/index.js
+++ basics/index.js
@@ -1,6 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
+
+const data = {
+  "id": 0,
+  "title": "string",
+  "price": 0.1,
+  "description": "string",
+  "category": "string",
+  "image": "http://example.com"
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -18,6 +28,14 @@ app.get("/sukriti", (req, res) => {
     res.send("<h2>Welcome to Sukriti's page</h2>")
 })
 
-app.listen(port, () => {
+app.get('/data', (req, res) => {
+    res.json(data)
+})
+
+app.get('/youtube', (req, res) => {
+    res.send("<h1>Welcome to my YouTube channel</h1>")
+})
+
+app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${port}`)
 })
