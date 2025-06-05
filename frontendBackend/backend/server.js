1dbef382befc2a7aa4300df4016489540acbbea8
Sourya <souryagupta2005@gmail.com>
2025-06-05 22:23:10 +0530

    learnt how to connect frontend and backend code togther and also learnt about axios

--- frontendBackend/backend/server.js
+++ frontendBackend/backend/server.js
@@ -6,6 +6,37 @@ app.get('/', (req, res) => {
     res.send("Server is Ready");
 });
 
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: "A Joke",
+            content: "This is a joke"
+        },
+        {
+            id: 2,
+            title: "Another Joke",
+            content: "This is another joke"
+        },
+        {
+            id: 3,
+            title: "A third Joke",
+            content: "This is a third joke"
+        },
+        {
+            id: 4,
+            title: "A fourth Joke",
+            content: "This is a fourth joke"
+        },
+        {
+            id: 5,
+            title: "A fifth Joke",
+            content: "This is a fifth joke"
+        }
+    ]
+    res.send(jokes);
+})
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
