faa32873cc76ccb02fca536e8030a89004777c42
Sourya <souryagupta2005@gmail.com>
2025-06-20 20:27:09 +0530

    learnt how to about how to make http serverss

--- nodeJs/server/index.js
+++ nodeJs/server/index.js
@@ -1,21 +1,33 @@
 const http = require("http")
 const fs = require("fs")
+const url = require("url")
 
 const myServer = http.createServer((req, res) => {
 
-
+    if (req.url === "/favicon.ico") return res.end()
     const log = `${Date.now()}: ${req.url} New Req Received \n`;
-    fs.appendFile('log.txt', log, (err, data) => {
-        switch(req.url) {
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl)
+    fs.appendFile('log.txt', log, (err, data) => {})
+
+        switch(myUrl.pathname) {
             case '/': res.end("<h1>HomePage</h1>")
             break;
-            case '/about': res.end("I am Sourya")
+            case '/about':{
+            const username = myUrl.query.myname
+            res.end(`Hi, ${username}`)
             break
+            }
+            case '/search': {
+            const search = myUrl.query.search_query
+            res.end("Here are your results for " + search)
+            break
+            }
             default:
                 res.end("404 Not Found!")
         }
         
-    })
+    
 
 })
 
