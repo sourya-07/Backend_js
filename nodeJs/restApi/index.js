2ce9d8ab34e8fef1ef92f75bd96570b6c7eabd87
Sourya <souryagupta2005@gmail.com>
2025-06-23 23:42:42 +0530

    Learnt about the POSTMAN for rest API's

--- nodeJs/restApi/index.js
+++ nodeJs/restApi/index.js
@@ -1,8 +1,12 @@
 const express = require("express")
+const fs = require('fs')
 const users = require("./MOCK_DATA.json")
 
 const app = express()
 const PORT = 8000
+
+// Middleware - Plugin
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/users', (req, res) => {
     const html = `
@@ -36,32 +40,27 @@ app
     })
     .delete((req, res) => {
         //Delete user with id
-        return res.json({ status: 'Pending' })
+        const id = Number(req.params.id)
+        const updatedUsers = users.filter((user) => user.id !== id)
+
+        users.length = 0
+        updatedUsers.forEach(user => users.push(user))
+
+        fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+            return res.json({ status: 'success', message: `User with id ${id} deleted.` })
+        })
     })
 
 
-
-.get("/api/users/:id", (req, res) => {
-    const id = Number(req.params.id)
-    const user = users.find((user) => user.id === id)
-    return res.json(user)
-})
-
 app.post('/api/users', (req, res) => {
     // TODO: Create new user
-    return res.json({ status: 'pending' })
+    const body = req.body
+    // console.log("Body", body)
+    users.push({ ...body, id: users.length + 1 })
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        return res.json({ status: 'success', id: users.length })
+    })
 })
-
-// app.patch('/api/users/:id', (req, res) => {
-//     // TODO: Edit the user with id
-//     return res.json({ status: 'pending' })
-// })
-
-// app.delete('/api/users/:id', (req, res) => {
-//     // TODO: Delete the user with id
-//     return res.json({ status: 'pending' })
-// })
-
 
 
 app.listen(PORT, () => console.log(`Server Started at Port ${PORT}`))
