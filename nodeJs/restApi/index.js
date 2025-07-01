23576918f7cd4c8f5251a64cc7d79bc99ffaa230
Sourya <souryagupta2005@gmail.com>
2025-07-01 22:19:08 +0530

    learnt about mongoose and Schema

--- nodeJs/restApi/index.js
+++ nodeJs/restApi/index.js
@@ -1,10 +1,43 @@
 const express = require("express")
 const fs = require('fs')
-const users = require("./MOCK_DATA.json")
-const { error } = require("console")
+const mongoose = require('mongoose')
+// const users = require("./MOCK_DATA.json")
 
 const app = express()
 const PORT = 8000
+
+
+// Connection 
+mongoose
+    .connect('mongodb://127.0.0.1:27017/youtube-app-1')
+    .then(() => console.log('MongoDB Connected'))
+    .catch((err) => console.log('Mongo Error', err))
+// Schema 
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    jonTitle: {
+        type: String,
+    },
+    gender: {
+        type: String,
+    },
+
+}, { timestamps: true })
+
+const User = mongoose.model("user", userSchema)
+
 
 // Middleware - Plugin
 app.use(express.urlencoded({ extended: false }))
@@ -27,10 +60,11 @@ app.use((req, res, next) => {
 //     next()
 // })
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
+    const allDbUsers = await User.find()
     const html = `
     <ul>
-        ${users.map((user) => `<li>First Name = ${user.first_name} and Last Name = ${user.last_name}</li>`
+        ${allDbUsers.map((user) => `<li>First Name = ${user.firstName} and Last Name = ${user.lastName} - ${user.email}</li>`
     ).join("")}
         
     </ul>
@@ -40,53 +74,53 @@ app.get('/users', (req, res) => {
 })
 
 // Rest Api :-
-app.get('/api/users', (req, res) => {
+app.get('/api/users', async (req, res) => {
+    const allDbUsers = await User.find()
+
     res.setHeader("X-MyName", 'Sourya') // This is our custom Header
     // Always add X to custom headers
     console.log(req.headers)
-    return res.json(users)
+    return res.json(allDbUsers)
 })
 
 
 
 app
     .route("/api/users/:id")
-    .get((req, res) => {
-        const id = Number(req.params.id)
-        const user = users.find((user) => user.id === id)
+    .get( async (req, res) => {
+        const user = await User.findById(req.params.id)
         if(!user) return res.status(404).json({ error: 'user not found' })
         return res.json(user)
     })
-    .patch((req, res) => {
+    .patch(async(req, res) => {
         //Edit user with id
-        return res.json({ status: 'Pending' })
+        await User.findByIdAndUpdate(req.params.id, { lastName: 'Changed'})
+        return res.json({ status: 'Success' })
     })
-    .delete((req, res) => {
+    .delete(async(req, res) => {
         //Delete user with id
-        const id = Number(req.params.id)
-        const updatedUsers = users.filter((user) => user.id !== id)
-
-        users.length = 0
-        updatedUsers.forEach(user => users.push(user))
-
-        fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
-            return res.json({ status: 'success', message: `User with id ${id} deleted.` })
-        })
+        await User.findByIdAndDelete(req.params.id)
+        return res.json({ status: 'Success' })
     })
 
 
-app.post('/api/users', (req, res) => {
+app.post('/api/users', async (req, res) => {
     // TODO: Create new user
     const body = req.body
     if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title){
         return res.status(400).json({ msg: 'All fields are req...' })
     }
 
-    // console.log("Body", body)
-    users.push({ ...body, id: users.length + 1 })
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
-        return res.status(201).json({ status: 'success', id: users.length })
+    const result = await User.create({
+        firstName: body.first_name,
+        lastName: body.last_name,
+        email: body.email,
+        gender: body.gender,
+        jobTitle: body.job_title,
     })
+
+    return res.status(201).json({ msg: 'Sucess' })
+    
 })
 
 
