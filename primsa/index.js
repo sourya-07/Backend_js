4c694c35cfcef8eb1e4cc7335f9882a98a79095d
Sourya <souryagupta2005@gmail.com>
2025-09-16 06:50:47 +0530

    Learnt about how to setup project using prisma

---
+++ primsa/index.js
@@ -0,0 +1,36 @@
+const { PrismaClient } = require('./generated/prisma')
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.ion',
+      posts: {
+        create: { title: 'Hello World' },
+      },
+      profile: {
+        create: { bio: 'I like turtles' },
+      },
+    },
+  })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+  console.log(allUsers, { depth: null })
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
